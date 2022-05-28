8cd74420e309eb709cce2a5d6ddcfd5653fa90bf
sriayu <sriaayuwlndr@gmail.com>
2022-05-29 01:13:50 +0700

    Mengedit tugas di index.js

--- index.js
+++ index.js
@@ -2,20 +2,21 @@
 // Tentukan tipe data dan struktur data yang menurut kalian tepat
 // Kalian bisa membuat variabel lain untuk dimasukkan ke dalam atribut user agar lebih mudah dibaca
 
-function education(idSekolah, namaSekolah, namacity, tahunGraduate) {
+function educationUsers(idSekolah, namaSekolah, namacity, tahunGraduate) {
     this.id = idSekolah,
     this.name = namaSekolah,
     this.city = namacity,
     this.graduate = tahunGraduate
 }
 
-let educationMonicaSD = new education('1','SD 01', 'Jakarta', '2016');
-let educationMonicaSMP = new education('2','SMP 01', 'Jakarta', '2019');
-let educationMonicaSMA = new education('3','SMA 03', 'Tangerang');
-let educationWendySD = new education('1','SD 02', 'Jakarta', '2010');
-let educationWendySMP = new education('2','SMP 03', 'Bogor', '2013');
-let educationWendySMA = new education('3','SMA 01', 'Surabaya', '2016');
-let educationWendyUniversitas = new education('4','Universitas Maju', 'Tangerang');
+let educationMonicaSD = new educationUsers('1','SD 01', 'Jakarta', '2016');
+let educationMonicaSMP = new educationUsers('2','SMP 01', 'Jakarta', '2019');
+let educationMonicaSMA = new educationUsers('3','SMA 03', 'Tangerang');
+let educationWendySD = new educationUsers('1','SD 02', 'Jakarta', '2010');
+let educationWendySMP = new educationUsers('2','SMP 03', 'Bogor', '2013');
+let educationWendySMA = new educationUsers('3','SMA 01', 'Surabaya', '2016');
+let educationWendyUniversitas = new educationUsers('4','Universitas Maju', 'Tangerang');
+
 
 let educationMonica = [educationMonicaSD, educationMonicaSMP, educationMonicaSMA];
 let educationWendy = [educationWendySD, educationWendySMP, educationWendySMA, educationWendyUniversitas];
@@ -23,15 +24,18 @@ let educationWendy = [educationWendySD, educationWendySMP, educationWendySMA, ed
 let favResMonica = new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]);
 let favResWendy = new Set(["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"]);
 
+let colorMonica = new Set(["Yellow","Pink","White","Purple"]);
+let colorWendy = new Set(["Blue","Black","Grey"]);
+
 const firstUser = {
     name: "Monica",
     gender: "Female",
     age: "17",
     email: "monica@dingdong.com",
-    color: ["Yellow","Pink","White","Purple"],
+    color: colorMonica,
     isHavePet: "Yes",
     favRes: favResMonica,
-    education: () => console.log(educationMonica),
+    education: educationMonica,
 };
 
 const secondUser = {
@@ -39,10 +43,10 @@ const secondUser = {
     gender: "Male",
     age: "23",
     email: "wendy@dingdong.com",
-    color: ["Blue","Black","Grey"],
-    isHavePet: "Yes",
+    color: colorWendy,
+    isHavePet: "No",
     favRes: favResWendy,
-    education: () => console.log(educationWendy),
+    education: educationWendy,
 };
 
 
