e05f8306c452b6ad77e6b828ee2400d9762be238
sriayu <sriaayuwlndr@gmail.com>
2022-05-29 13:03:36 +0700

    Ganti Isi Restaurarant

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ let educationWendyUniversitas = new educationUsers('4','Universitas Maju', 'Tang
 let educationMonica = [educationMonicaSD, educationMonicaSMP, educationMonicaSMA];
 let educationWendy = [educationWendySD, educationWendySMP, educationWendySMA, educationWendyUniversitas];
 
-let favResMonica = new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Bento", "Eggy", "Padang", "Tteok", "Sushi", "Sushi"]);
-let favResWendy = new Set(["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Pancake", "Eggy"]);
+let favResMonica = new Set(["Bento", "Sushi", "Pancake", "Eggy", "Tempura", "Padang", "Tteok"]);
+let favResWendy = new Set(["Tempura", "Bento", "Sushi", "Pancake", "Padang", "Katsu", "Geprek", "Eggy"]);
 
 let colorMonica = new Set(["Yellow","Pink","White","Purple"]);
 let colorWendy = new Set(["Blue","Black","Grey"]);
